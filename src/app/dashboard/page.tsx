be213e38e2cf2b847aeef74c2fc14143baf05f5a
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-01-08 11:50:44 +0600

    patents component created

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,23 @@
-const Dashboard = () => {
+// const Dashboard = () => {
+//     return (
+//       <div>
+//         <h1 className="text-2xl font-bold">Welcome to the Hospital Dashboard</h1>
+//         <p>Manage hospital food, delivery systems, and more from here.</p>
+//       </div>
+//     );
+//   };
+
+//   export default Dashboard;
+
+import Sidebar from '@/Components/Dashboard/Sidebar';
+import React from 'react';
+
+const page = () => {
     return (
-      <div>
-        <h1 className="text-2xl font-bold">Welcome to the Hospital Dashboard</h1>
-        <p>Manage hospital food, delivery systems, and more from here.</p>
-      </div>
+        <div>
+            <Sidebar />
+        </div>
     );
-  };
-  
-  export default Dashboard;
-  
+};
+
+export default page;
